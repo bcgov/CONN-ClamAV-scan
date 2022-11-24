00229c3c76d56e86863942170f95e70f75d74bd2
VB-at-Bis <100612619+VB-at-Bis@users.noreply.github.com>
2022-11-24 09:34:39 -0800

    Lambda layer for ClamAV virus scan (#5)
    * Add compliance audit file
    
    Signed-off-by: repo-mountie <pathfinder@gov.bc.ca>
    
    * feat: ClamAV lambda layer
    
    * fix: added missing libraries and removed initial data files
    
    * chore: added compliance doc
    
    * fix: updated readme
    
    * fix: updated readme
    
    Signed-off-by: repo-mountie <pathfinder@gov.bc.ca>
    Co-authored-by: repo-mountie[bot] <44246292+repo-mountie[bot]@users.noreply.github.com>
    Co-authored-by: VB <vba66a@gmail.com>

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf ./layer
+mkdir layer
+
+docker build -t clamav -f Dockerfile .
+docker run --name clamav clamav
+docker cp clamav:/home/build/clamav_lambda_layer.zip .
+
+docker rm clamav
+
+mv clamav_lambda_layer.zip ./layer
+
+pushd layer
+unzip -n clamav_lambda_layer.zip
+popd
